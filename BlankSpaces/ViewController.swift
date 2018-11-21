7604199250b5c06c040d0fd6ec0356a7dff718f7
Jordan Pichler <j.pichler@icloud.com>
2018-11-21 16:43:28 +0100

    Initial Commit

---
+++ BlankSpaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlankSpaces
+//
+//  Created by Jordan Pichler on 21/11/2018.
+//  Copyright © 2018 Jordan A. Pichler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
