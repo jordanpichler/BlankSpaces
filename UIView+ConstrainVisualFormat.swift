6eb2c0bf4adce146d38dc8c45aaa428c945fd275
Jordan Pichler <j.pichler@icloud.com>
2018-11-22 15:15:11 +0100

    Add visual constraint format extension

---
+++ UIView+ConstrainVisualFormat.swift
@@ -0,0 +1,29 @@
+//
+//  UIView+ConstrainVisualFormat.swift
+//  BlankSpaces
+//
+//  Created by Jordan Pichler on 20/06/2018.
+//  Copyright © 2018 Jordan A. Pichler. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        
+        var viewDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format,
+                                                           options: NSLayoutConstraint.FormatOptions(),
+                                                           metrics: nil,
+                                                           views: viewDictionary))
+
+    }
+}
