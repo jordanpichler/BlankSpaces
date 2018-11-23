c21277f082531966bc4b304e960cec21a74b43b5
Jordan Pichler <j.pichler@icloud.com>
2018-11-23 14:33:18 +0100

    Invoke implementation to fetch server data

--- BlankSpaces/MainViewController.swift
+++ BlankSpaces/MainViewController.swift
@@ -12,7 +12,8 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let networker = NetworkingManager()
+        networker.fetchLessons()
         
     }
 
